3bf827c577d46a263120702c60aecd6fd3325c71
EvilGns <71857780+EvilGns@users.noreply.github.com>
2023-11-06 20:12:34 +0530

    Update access.py

--- access.py
+++ access.py
@@ -1,8 +1,7 @@
 import boto3
 
 # Hardcoded AWS Access Key (Replace with your real key)
-aws_access_key = 'AKIAEXAMPLEACCESSKEY'
-aws_secret_key = 'YourSecretKeyHere'
+
 
 # Initialize an AWS S3 client
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
