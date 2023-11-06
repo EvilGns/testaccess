097a646a18c7bf7ba0f3876e4c955b91dde472a7
EvilGns <71857780+EvilGns@users.noreply.github.com>
2023-11-06 20:21:29 +0530

    Update access.py

--- access.py
+++ access.py
@@ -1,2 +1,13 @@
 import boto3
 
+# Hardcoded AWS Access Key (Replace with your real key)
+aws_access_key = 'AKIAEXAMPLEACCESSKEY'
+aws_secret_key = 'YourSecretKeyHere'
+
+# Initialize an AWS S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+
+# List buckets
+buckets = s3.list_buckets()
+for bucket in buckets['Buckets']:
+    print(f'Bucket Name: {bucket["Name"]}')
